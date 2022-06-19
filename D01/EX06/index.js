291f54170cda8aeba1ad69f60517ee382e88aa07
Rotem Shuval <rotem@Rotems-MacBook-Air.local>
2022-06-19 14:58:29 +0300

    dailies done! :octocat:

--- D01/EX06/index.js
+++ D01/EX06/index.js
@@ -1,3 +1,21 @@
 function moduloEx6(number, comparer) {
-  //your code
+  const modulo = number % comparer;
+  let response = "";
+
+  if (number > comparer) {
+    response = `The number ${number} is bigger than ${comparer}`;
+    if (modulo > 0) {
+      response += `. But the modulo of ${number} % ${comparer} is ${modulo}`;
+    }
+  } else if (number === comparer) {
+    response = `The number ${number} is equal to ${comparer}`;
+  } else {
+    response = `The number ${number} is less than ${comparer}`;
+  }
+
+  return response;
 }
+
+const output = document.getElementById("output");
+
+output.innerText = moduloEx6(8, 3);
--- D01/EX07/index.js
+++ D01/EX07/index.js
@@ -1,7 +1,27 @@
-function getRandomInt(max) {
-  //your code
+function getRandomInt() {
+  return Math.floor(Math.random() * 100 + 1);
 }
 
 function moduloEx7() {
-  //your code
+  const number = getRandomInt();
+  const comparer = getRandomInt();
+  const modulo = number % comparer;
+  let response = "";
+
+  if (number > comparer) {
+    response = `The number ${number} is bigger than ${comparer}`;
+    if (modulo > 0) {
+      response += `. But the modulo of ${number} % ${comparer} is ${modulo}`;
+    }
+  } else if (number === comparer) {
+    response = `The number ${number} is equal to ${comparer}`;
+  } else {
+    response = `The number ${number} is less than ${comparer}`;
+  }
+
+  return response;
 }
+
+const output = document.getElementById("output");
+
+output.innerText = moduloEx7();
